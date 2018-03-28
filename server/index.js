183c150bd74be2caf6309c7ffcc8f7bd0fc0d4d9
Nuke KitK <inmemory_8566@hotmail.com>
2018-03-28 14:37:00 +0700

    Trying to fix database connection

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
-
+const pg = require('pg');
 const PORT = process.env.PORT || 5000;
+const { Client } = require('pg');
 
 // Multi-process to utilize all CPU cores.
 if (cluster.isMaster) {
@@ -19,6 +20,12 @@ if (cluster.isMaster) {
   });
 
 } else {
+
+  const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+  });
+  client.connect();  
   const app = express();
 
   // Priority serve any static files.
@@ -28,6 +35,22 @@ if (cluster.isMaster) {
   app.get('/api', function (req, res) {
     res.set('Content-Type', 'application/json');
     res.send('{"message":"Hello from the custom server!"}');
+  });
+
+  // Test db
+  app.get('/db', function(req, res){
+    pg.connect(process.env.DATABASE_URL+"?ssl=true",function(err, client, done){
+      client.query('SELECT * FROM test_table', function(err, result){
+        done();
+        if(err){
+            console.error(err)
+            res.send("ERRORRRR!!!! " + err)
+        } else{
+            res.render('pages/db', {results: result.rows})
+        }
+        client.end();
+      })  
+    })
   });
 
   // All remaining requests return the React app, so it can handle routing.
